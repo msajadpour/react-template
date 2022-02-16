3c505c38e073a62dca9f7a847cdb15140c3b248c
msajadpour <msajadpour20@gmail.com>
2022-02-17 01:01:33 +0330

    fist publish

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Home from "./pages/home/home";
+
+//Main Component
+function App() {
+
+    return (
+        <>
+            <Home />
+        </>
+    );
+}
+
+export default App;
---
+++ src/utils.js
@@ -0,0 +1,3 @@
+export const apiUrl = process.env.REACT_APP_API_URL;
+
+// export const apiUrl = 'http://localhost:8010/api/v1';
